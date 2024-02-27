1364a29d3c73dbda2c535db05b3acc55c9b558a9
MegaVirus93 <MegaVirus93@gmail.com>
2024-02-27 08:21:44 +0300

    2.2.2 Java pre-project. Задача 2.2.2. MVC-приложение. Исправлено: 1) внедрение зависимостей

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,12 @@ import web.service.CarServiceImpl;
 @Controller
 public class CarController {
 
-    private final CarService carService = new CarServiceImpl();
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
